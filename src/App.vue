a264d8c2eeb76a3e49a73b2aec564d15b23f6495
REInVent650 <78056322+REInVent650@users.noreply.github.com>
2021-04-10 15:08:30 +1200

    Update App.vue
    add script tags and change styles from stylus to css

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,31 @@
   </div>-->
   <router-view />
 
-  <h1></h1>
+  <h1>header 1 </h1>
 </template>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "weekendview",
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$data.title)
+    }
+  },
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
